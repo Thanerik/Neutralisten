d7531d006ecec8f12becff4209a1f9ad31b19706
Troels Madsen <troels@mydahlmadsen.dk>
2014-11-26 12:58:52 +0100

    Updating it to verison 1.5

--- bot.js
+++ bot.js
@@ -11,6 +11,15 @@ var N = require('./neutral.json');
 // This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
 var Search = {q: "#dkpol", count: 50, result_type: "recent"};
 
+function cleanTweet(data) {
+  var ret = data.replace(/&quot;/g, "\"");
+      ret = ret.replace(/&apos;/g, "'");
+      ret = ret.replace(/&amp;/g, "&");
+      ret = ret.replace(/&gt;/g, ">");
+      ret = ret.replace(/&lt;/g, "<");
+      return ret;
+}
+
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {
     T.get('search/tweets', Search, function (error, data) {
@@ -34,6 +43,8 @@ function retweetLatest() {
         	//  Print all the outputs - to see it works!
             	//console.log(tweetData[i]);
             var whom = tweetData[i].user.screen_name;
+            var tweetID = tweetData[i].id;
+            var tweetRT = tweetData[i].retweeted; // returns true or false
             var tweetOrd = tweetData[i].text.split(" ");
             var sprog = tweetData[i].lang;
             var neutral = 0;
@@ -41,13 +52,13 @@ function retweetLatest() {
             var basestr = 0;
             var count = 0;
 
-            if(whom !== "Neutralisten" && sprog === "da") {
+            if(whom !== "Neutralisten" && sprog === "da" && !tweetRT) {
 
             	ord: for (var o = tweetOrd.length - 1; o >= 0; o--) {
 
-
                     // Opsætte regler for orde vi springer over - og hvis RT/MT, så drop hele tweet'en
                     if(tweetOrd[o] === "RT" || tweetOrd[o] === "MT" ) { continue tweet; }
+                    // Eller spring over orde som ikke skal ersattes
                     if(tweetOrd[o].toLowerCase() === "frankrig" ||
                        tweetOrd[o].toLowerCase() === "fattigdom" ||
                        tweetOrd[o].toLowerCase() === "fattigdommen" ||
@@ -59,6 +70,10 @@ function retweetLatest() {
                        tweetOrd[o].toLowerCase() === "bekrige" ||
                        tweetOrd[o].toLowerCase() === "bekriger" ||
                        tweetOrd[o].toLowerCase() === "forsvare" ||
+                       tweetOrd[o].toLowerCase() === "forsvarlig" ||
+                       tweetOrd[o].toLowerCase() === "uforsvarlig" ||
+                       tweetOrd[o].toLowerCase() === "forsvarlige" ||
+                       tweetOrd[o].toLowerCase() === "uforsvarlige" ||
                        tweetOrd[o].toLowerCase() === "forsvarer") { continue ord; }
 
             		var hashtag = tweetOrd[o].indexOf("#"); // returer 0 hvis har match - ellers -1
@@ -96,7 +111,7 @@ function retweetLatest() {
 
                 if(totalAnslag > 140) {
                     var charLeft = 140 - whom.length - ekstrategn;
-                    var halfway = tweetOutput.length/2;
+                    var halfway = charLeft/2;
                     var start = tweetOutput.length - charLeft;
 
                     if(halfway+basestr >= charLeft && basestr-halfway <= 0) {
@@ -116,6 +131,9 @@ function retweetLatest() {
                     }
                 }
 
+                //Renser vores tweet for html-koder der repræsenterer symboler som <, >, &, " og '
+                tweetOutput = cleanTweet(tweetOutput);
+
                 // Den rigtige output.
                 tweetOutput = "@" + whom + ": " + tweetOutput + " #DKpol";
             	
@@ -123,8 +141,8 @@ function retweetLatest() {
                 console.log("Before: " + tweetData[i].text );
                 console.log("After: " + tweetOutput + " (" + neutral + " point)" );
                 console.log("---------");
-            	
-                T.post('statuses/update', { status: tweetOutput }, function(err, data, response) {
+
+                T.post('statuses/update', { status: tweetOutput, in_reply_to_status_id: tweetID }, function(err, data, response) {
             		console.log(err);
                     if(!err) {
                         count += 1; // Tæller hvor mange tweets vi sender. Maks 2 pr. session!
